4e7d238c4fa62d5741a0cea0bdf967ad5e83c056
Ethan Lu <ethanlooo@outlook.com>
2021-07-20 21:19:02 +0800

    🐳 chore: import pycg for call graph

---
+++ code/banana.py
@@ -0,0 +1,31 @@
+class Banana:
+
+    def eat(self):
+        pass
+
+
+class Person:
+
+    def __init__(self):
+        self.no_bananas()
+
+    def no_bananas(self):
+        self.bananas = []
+
+    def add_banana(self, banana):
+        self.bananas.append(banana)
+
+    def eat_bananas(self):
+        [banana.eat() for banana in self.bananas]
+        self.no_bananas()
+
+
+def main():
+    person = Person()
+    for a in range(10):
+        person.add_banana(Banana())
+    person.eat_bananas()
+
+
+if __name__ == '__main__':
+    main()
